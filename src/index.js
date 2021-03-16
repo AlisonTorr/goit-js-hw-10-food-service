4b2ba8a59b7ceafd03b3bc0f76af32f74b93d7a4
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-03-16 21:16:52 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import menuListMarkup from "./menu";
 
 const menuList = document.querySelector(".js-menu");
 menuList.appendChild(menuListMarkup);
+
+const themeCheckbox = document.querySelector("#theme-switch-toggle");
+
+themeCheckbox.addEventListener("change", onCheckboxToggle);
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const onCheckboxToggle = () => {
+  if (body.classList.has("Theme.LIGHT")) {
+    body.classList.remove("Theme.LIGHT");
+    body.classList.add("Theme.DARK");
+  } else {
+    body.classList.add("Theme.LIGHT");
+  }
+};

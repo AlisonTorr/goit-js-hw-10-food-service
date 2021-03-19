ddfca3cfa9de9ee41a65eb9ccdd009bc057ff25f
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-03-19 17:29:57 +0300

    Delete src directory

--- src/menu.js
+++
@@ -1,9 +0,0 @@
-import menuTpl from './tamplates/menu.hbs';
-import menuItems from './menu.json';
-
-function createMenuListMarkup(menuItems) {
-  return menuItems.map(item => menuTpl(item)).join('');
-}
-
-const menuListMarkup = createMenuListMarkup(menuItems);
-export default menuListMarkup;
